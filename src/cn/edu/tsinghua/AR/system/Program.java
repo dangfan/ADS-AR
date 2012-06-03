7c5ae5e6e1cdc95f7212ea3d868fc56708ca671c
terro <dangf09@gmail.com>
2012-06-03 10:31:10 +0800

    add docs

--- src/cn/edu/tsinghua/AR/system/Program.java
+++ src/cn/edu/tsinghua/AR/system/Program.java
@@ -30,11 +30,63 @@ class Pair implements Comparable<Pair> {
 
 public class Program {
 
+    public static String normalize(String path) {
+
+        if (path == null)
+            return null;
+
+        // Create a place for the normalized path
+        String normalized = path;
+
+        if (normalized.equals("/."))
+            return "/";
+
+        // Add a leading "/" if necessary
+        if (!normalized.startsWith("/"))
+            normalized = "/" + normalized;
+
+        // Resolve occurrences of "//" in the normalized path
+        while (true) {
+            int index = normalized.indexOf("//");
+            if (index < 0)
+                break;
+            normalized = normalized.substring(0, index) +
+                normalized.substring(index + 1);
+        }
+
+        // Resolve occurrences of "/./" in the normalized path
+        while (true) {
+            int index = normalized.indexOf("/./");
+            if (index < 0)
+                break;
+            normalized = normalized.substring(0, index) +
+                normalized.substring(index + 2);
+        }
+
+        // Resolve occurrences of "/../" in the normalized path
+        while (true) {
+            int index = normalized.indexOf("/../");
+            if (index < 0)
+                break;
+            if (index == 0)
+                return (null);  // Trying to go outside our context
+            int index2 = normalized.lastIndexOf('/', index - 1);
+            normalized = normalized.substring(0, index2) +
+                normalized.substring(index + 3);
+        }
+
+        // Return the normalized path that we have completed
+        return (normalized);
+
+    }
+	
 	public static void main(String[] args) {
-		Preprocess.start(args[0]);
-		Corpus corpus = LdaEstimate.lda(0.25f, 10, "data/mult.dat", "data");
-		CTREstimate.ctr(corpus, 1.0, 0.01, 0.01, 100, 200, 200, 5551, 16980, args[1]);
-		DoTest(args[2]);
+		System.out.println(normalize("bar.jsp?somepar=someval&par=/../WEB-INF/web.xml"));
+//		Preprocess.start(args[0]);
+//		Corpus corpus = LdaEstimate.lda(0.25f, 200, "data/mult.dat", "data");
+//		CTREstimate.ctr(corpus, 1.0, 0.01, 0.01, 100, 200, 200, 5551, 16980, args[1]);
+//		DoTest(args[2]);
+		
 	}
 
 	private static void DoTest(String fileName) {
